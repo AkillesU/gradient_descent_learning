f4be3748e531b6ab297078babfc740a9e8c62f0b
AkillesU <a.rechardt@gmail.com>
2023-10-31 14:20:23 +0000

    Fix graph script

--- plots/hmm_plot.py
+++ plots/hmm_plot.py
@@ -1,43 +1,57 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import pickle
+import numpy as np
 
-with open("../hmm/hmm_results/best_model.pkl", "rb") as file:
+with open("../hmm/hmm_results/best_model_states_4.pkl", "rb") as file:
     model = pickle.load(file)
 
-n_states = model.n_states
-transition_probabilities = model.transition_prob_
-emission_probabilities = model.emission_prob_
-starting_probabilities = model.start_prob_
+n_states = model.n_components
+transition_probabilities = np.around(model.transmat_,3)
+emission_probabilities = np.around(model.emissionprob_,3)
+starting_probabilities = np.around(model.startprob_,3)
 
+print(transition_probabilities,
+      emission_probabilities,
+      starting_probabilities,
+      n_states)
 # Create graph
 G = nx.DiGraph()
 
-# Add nodes
-for s in range(n_states):
-    G.add_node(s)
+# Add nodes with starting probabilities
+for i, prob in enumerate(starting_probabilities):
+    G.add_node(i, starting_prob=prob, emissions=emission_probabilities[i])
 
-# Add edges for transitions
-for i in range(n_states):
-    for j in range(n_states):
-        G.add_edge(i, j, weight=transition_probabilities[i, j])
+# Add edges with transition probabilities
+for i, row in enumerate(transition_probabilities):
+    for j, prob in enumerate(row):
+        if prob > 0:
+            G.add_edge(i, j, weight=prob)
 
 # Draw graph
-pos = nx.spring_layout(G)
+pos = nx.circular_layout(G)
 plt.figure(figsize=(10, 8))
-nx.draw_networkx_nodes(G, pos, node_size=3000)
-nx.draw_networkx_edges(G, pos)
-nx.draw_networkx_labels(G, pos)
-labels = {(i, j): '{:.2f}'.format(transition_probabilities[i][j]) for i, j in G.edges()}
-nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+nx.draw(G, pos, with_labels=False, node_size=3000, node_color='skyblue', font_weight='bold', arrowsize=20)
 
-# Display emission probabilities
-for i, (x, y) in pos.items():
-    plt.text(x, y - 0.15, f'E: {emission_probabilities[i]}', horizontalalignment='center', verticalalignment='center')
+# Draw starting probabilities inside nodes
+for i, prob in enumerate(starting_probabilities):
+    plt.text(*pos[i], f'{prob:.2f}', fontsize=12, ha='center', va='center')
 
-# Display starting probabilities (optional)
-for i, (x, y) in pos.items():
-    plt.text(x, y + 0.15, f'S: {starting_probabilities[i]:.2f}', horizontalalignment='center', verticalalignment='center')
+# Draw transition probabilities on edges
+edge_labels = {(i, j): f'{w["weight"]:.2f}' for i, j, w in G.edges(data=True)}
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+for (i, j), label in edge_labels.items():
+    if i == j:  # If it's a self loop
+        x, y = pos[i]
+        plt.text(x-0.1, y+0.2, label, fontsize=10, ha='center', va='center')
+
+# Draw emission probabilities next to nodes
+for i, emissions in enumerate(emission_probabilities):
+    plt.text(pos[i][0], pos[i][1]-0.15, f'{emissions}', fontsize=10, ha='center', va='center')
 
 plt.title('Hidden Markov Model Visualization')
 plt.show()
+
+
+
