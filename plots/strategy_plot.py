5c009241d0fb964abdba1f420c21113c2c33c8ef
AkillesU <a.rechardt@gmail.com>
2023-11-04 14:24:53 +0000

    Add exclusion and update plot title

--- plots/strategy_plot.py
+++ plots/strategy_plot.py
@@ -4,6 +4,11 @@ import numpy as np
 
 df = pd.read_csv("../likelihood_model/likel_results/model_fit_results.csv")
 
+df_exclude = pd.read_csv("../data/exclusions.csv")
+
+# Exclude participants
+df = df[~df['id'].isin(df_exclude['id'])]
+
 n_participants = int(len(df)/10)
 
 fig, ax = plt.subplots(1, 2, figsize=(16, 6))
@@ -21,7 +26,7 @@ for i, strategy in enumerate(strategies):
 
     ax[0].plot(trial_counts.index, trial_counts.values, color=colors[i], label=strategy)
 
-ax[0].set_title("Strategies across trials")
+ax[0].set_title(f"Strategies across trials (n = {n_participants})")
 ax[0].set_xlabel("Trial")
 ax[0].set_ylabel("Count")
 ax[0].set_xticks(np.arange(1, 11))
@@ -31,7 +36,7 @@ ax[0].grid(True, which='both', linestyle='--', linewidth=0.5)
 # Plot 2: "Total strategy incidence"
 total_strategy_counts = df['best_strategy_guess'].value_counts()
 total_strategy_counts.reindex(strategies).plot(kind='bar', ax=ax[1], color=colors)
-ax[1].set_title("Total strategy incidence")
+ax[1].set_title(f"Total strategy incidence (n = {n_participants})")
 ax[1].set_xlabel("Strategy")
 ax[1].set_ylabel("Count")
 ax[1].grid(True, axis='y', linestyle='--', linewidth=0.5)
