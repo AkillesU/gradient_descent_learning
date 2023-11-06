9eb0ed51716499eda779aa4bddb99cbb60c0b5a2
AkillesU <a.rechardt@gmail.com>
2023-11-06 12:31:43 +0000

    Change delimiter for "cleaned_pilotdata.csv"

--- likelihood_model/model_fitting.py
+++ likelihood_model/model_fitting.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scipy import optimize
 
 # Read participant data
-part_data = pd.read_csv("../data/cleaned_pilotdata.csv", delimiter=";")
+part_data = pd.read_csv("../data/cleaned_pilotdata.csv", delimiter=",")
 # Read code file
 code_file = pd.read_csv("../data/fullcode.csv", delimiter=";")
 
